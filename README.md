21fc0c858d701eb8c4a564dcaab4160d76086278
Najeem Muhammed <najeem@gmail.com>
2023-12-02 18:06:19 +0100

    Updated readme and usage

--- README.md
+++ README.md
@@ -54,6 +54,49 @@ Look in to `usage.py` for a nested panels example.
 pip install dash-resizable-panels
 ```
 
+## Attributes
+
+### PanelGroup
+
+| Attribute | Description | Type | Default |
+| --- | --- | --- | --- |
+| id | The ID used to identify this component in Dash callbacks | string | |
+| children | The children of this component | Dash components | |
+| direction | The direction of the panels. Can be either `horizontal` or `vertical` | string | `horizontal` |
+| className | The class name of the component | string | |
+| style | The style of the component | dict | |
+| autoSaveId | The ID used to save the group arrangement via local storage | string | |
+
+
+### Panel
+
+| Attribute | Description | Type | Default |
+| --- | --- | --- | --- |
+| id | The ID used to identify this component in Dash callbacks | string | |
+| children | The children of this component | Dash components | |
+| className | The class name of the component | string | |
+| defaultSizePixels | The default size of the panel in pixels | int | `0` |
+| defaultSizePercentage | The default size of the panel as a percentage of the parent panel group | int | `0` |
+| minSizePixels | The minimum size of the panel in pixels | int | `0` |
+| minSizePercentage | The minimum size of the panel as a percentage of the parent panel group | int | `0` |
+| maxSizePixels | The maximum size of the panel in pixels | int | `0` |
+| maxSizePercentage | The maximum size of the panel as a percentage of the parent panel group | int | `0` |
+| order | The order of the panel within group; Required for groups with conditionally rendered panels | int | `0` |
+| collapsible | Whether the panel can be collapsed | bool | `False` |
+| collapsedSizePixels | The size of the panel when collapsed | int | `0` |
+| collapsedSizePercentage | The size of the panel when collapsed as a percentage of the parent panel group | int | `0` |
+| style | The style of the component | dict | |
+
+### PanelResizeHandle
+
+| Attribute | Description | Type | Default |
+| --- | --- | --- | --- |
+| id | The ID used to identify this component in Dash callbacks | string | |
+| children | The children of this component | Dash components | |
+| className | The class name of the component | string | |
+| style | The style of the component | dict | |
+| disable | Whether the resize handle is disabled | bool | `False` |
+
 ## Sponsor
 
 If you like this project and want to support it, please consider becoming a sponsor of the parent project
--- usage.py
+++ usage.py
@@ -3,53 +3,51 @@ from dash import Dash, html
 
 app = Dash(__name__)
 
-app.layout = html.Div([
-    PanelGroup(
-        id='panel-group',
-        children=[
-            Panel(
-                id='panel-1',
-                children=[
-                    html.Div('Panel 1')
-                ],
-                defaultSizePercentage=20,
-                minSizePercentage=10
-            ),
-            PanelResizeHandle(html.Div(' ', style={"backgroundColor": "grey", "height": "100%", "width": "5px"})),
-            Panel(
-                id='panel-2',
-                children=[
-                    PanelGroup(
-                        id='panel-group-2',
-                        children=[
-                            Panel(
-                                id='panel-2-1',
-                                children=[
-                                    html.Div('Panel 2-1')
-                                ]
-                            ),
-                            PanelResizeHandle(html.Div(' ', style={"backgroundColor": "grey", "width": "100%", "height": "5px"})),
-                            Panel(
-                                id='panel-2-2',
-                                children=[
-                                    html.Div('Panel 2-2')
-                                ]
-                            )
-                        ], direction='vertical'
-                    )
-                ]
-            ),
-            PanelResizeHandle(html.Div(' ', style={"backgroundColor": "grey", "height": "100%", "width": "5px"})),
-            Panel(
-                id='panel-3',
-                children=[
-                    html.Div('Panel 3')
-                ]
-            )
-        ], direction='horizontal'
-    )
-], style={"height": "100vh"})
+# Styles are defined in assets/style.css
+app.layout = html.Div(
+    className="container",
+    children=[
+        PanelGroup(
+            id="panel-group",
+            children=[
+                Panel(
+                    id="panel-1",
+                    children=[html.Div("Panel 1")],
+                    defaultSizePercentage=20,
+                    minSizePercentage=15,
+                    collapsible=True,
+                ),
+                PanelResizeHandle(html.Div(className="resize-handle-horizontal")),
+                Panel(
+                    id="panel-2",
+                    children=[
+                        PanelGroup(
+                            id="panel-group-2",
+                            children=[
+                                Panel(id="panel-2-1", children=["Panel 2-1"]),
+                                PanelResizeHandle(
+                                    html.Div(className="resize-handle-vertical")
+                                ),
+                                Panel(id="panel-2-2", children=[html.Div("Panel 2-2")]),
+                            ],
+                            direction="vertical",
+                        )
+                    ],
+                    minSizePercentage=50,
+                ),
+                PanelResizeHandle(html.Div(className="resize-handle-horizontal")),
+                Panel(
+                    id="panel-3",
+                    children=[html.Div("Panel 3")],
+                    defaultSizePercentage=20,
+                    minSizePercentage=10,
+                ),
+            ],
+            direction="horizontal",
+        )
+    ],
+)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
